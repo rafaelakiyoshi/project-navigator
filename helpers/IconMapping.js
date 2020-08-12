fc2f3b4298b76ef8646e3320eb35202bebf65224
Rafael Akiyoshi <rafah94@hotmail.com>
2020-08-11 21:10:27 -0400

    creating a iconmap hash to select icon

---
+++ helpers/IconMapping.js
@@ -0,0 +1,25 @@
+const icons = {
+  js: "/js.svg",
+  babel: "/babel.svg",
+  babelrc: "/babel.svg",
+  css: "/css.svg",
+  gitignore: "/git.svg",
+  dockerignore: "/docker.svg",
+  docker: "/docker.svg",
+  gulpfile: "/gulp.svg",
+  json: "/json.svg",
+  less: "/less.svg",
+  md: "/markdown.svg",
+  svg: "/svg.svg",
+  html: "/html.svg",
+  ts: "/typescript.svg",
+  tsx: "/typescript.svg",
+  yarn: "/yarn.svg"
+};
+const IconMapping = name => {
+  const fragments = name.split(".");
+  const icon = icons[fragments[fragments.length - 1]] || icons[fragments[fragments.length - 2]];
+  return icon;
+};
+
+export default IconMapping;
